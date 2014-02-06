a0d436275055acadf9c082245a5129314ac2bc42
Alexander Pronin <pronin.alx@gmail.com>
2014-02-06 15:17:15 +0200

    Initial Commit

---
+++ 10NotificationsTestHW01/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  10NotificationsTestHW01
+//
+//  Created by Pronin Alexander on 06.02.14.
+//  Copyright (c) 2014 Pronin Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RITAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RITAppDelegate class]));
+    }
+}
---
+++ 10NotificationsTestHW01Tests/_0NotificationsTestHW01Tests.m
@@ -0,0 +1,34 @@
+//
+//  _0NotificationsTestHW01Tests.m
+//  10NotificationsTestHW01Tests
+//
+//  Created by Pronin Alexander on 06.02.14.
+//  Copyright (c) 2014 Pronin Alexander. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _0NotificationsTestHW01Tests : XCTestCase
+
+@end
+
+@implementation _0NotificationsTestHW01Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
